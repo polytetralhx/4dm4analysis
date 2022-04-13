95c71acc0bec0134a22f0bd8243c24286026651b
IndexError <lkycst@gmail.com>
2022-04-13 23:47:19 +0700

    + Hypothesis Testing

--- mappoolanalysis.py
+++ mappoolanalysis.py
@@ -2,45 +2,62 @@ import pandas as pd
 import numpy as np
 from scipy.stats import chi2, norm, t, f
 
-chi2_invcdf = chi2.isf
-norm_invcdf = norm.ppf
-norm_cdf = norm.cdf
-f_invcdf = f.ppf
-f_cdf = f.cdf
+t_cdf = t.cdf
+t_invcdf = t.ppf
 
 logit_dataset = pd.read_csv('4dm_logit.csv')
 
-"""Ignore the code here it's just my failed attempt on Homogenity test"""
 
-# def distribute(seq: np.ndarray, ranges: np.ndarray):
-#     distributed_data = []
-#     for i, j in zip(ranges[:-1], ranges[1:]):
-#         distributed_data.append(len(seq[(i <= seq) & (seq < j)]))
-#     return np.array(distributed_data) / len(seq)
+# some weird mappool analysis idk
 
-# def test_same_distribution(round1: str, type1: str, round2: str, type2: str, alpha: float, n_freq: int = 20):
-#     col1 = round1 + "_" + type1
-#     col2 = round2 + "_" + type2
-#     seq1 = logit_dataset[col1]
-#     seq1 = seq1[pd.isna(seq1) == False].values
-#     seq2 = logit_dataset[col2]
-#     seq2 = seq2[pd.isna(seq2) == False].values
-#     # standardize
-#     std_seq1 = (seq1 - np.mean(seq1)) / np.std(seq1)
-#     std_seq2 = (seq2 - np.mean(seq2)) / np.std(seq2)
-    
-#     all_data = np.concatenate((std_seq1, std_seq2))
-#     ranges = np.linspace(np.min(all_data), np.max(all_data), n_freq)
-#     df = n_freq - 1
+# plan 
+# Normal Distribution MODCHECK ?
+# Mean Hypothesis testing with alpha = 0.05
+# Variance test chi^2 something soemthing alpha = 0.05
 
-#     distributed_seq1 = distribute(std_seq1, ranges)
-#     distributed_seq2 = distribute(std_seq2, ranges)
-#     distributed_allseq = distribute(all_data, ranges)
+def mean_hypothesis_test(mean1, std1, n1, mean2, std2, n2, alpha=0.05):
+    """
+    Construct a hypothesis testing where null hypothesis is
+    H0 : mu1 = mu2
+    Ha : mu1 != mu2
+    """
+    std_ = np.sqrt(std1 ** 2 / n1 + std2 ** 2 / n2)
+    difference = mean1 - mean2
+    df = ((std1 ** 2 / n1 + std2 ** 2 / n2) ** 2) / ((std1 ** 2 / n1) ** 2 / (n1 - 1) + (std2 ** 2 / n2) ** 2 / (n2 - 1))
+    t_stat = difference / std_
+    t_alpha = t_invcdf(alpha / 2, df)
 
-#     x2_stat = np.sum(((distributed_seq1 - distributed_allseq) ** 2) / distributed_allseq) + np.sum(((distributed_seq2 - distributed_allseq) ** 2) / distributed_allseq)
-#     x2_alpha = chi2_invcdf(alpha, df)
+    return t_cdf(-np.abs(t_stat), df), t_stat < t_alpha or t_stat > -t_alpha
 
-#     return x2_stat, x2_stat > x2_alpha
+def hypothesis_raw_dataset(data1: np.ndarray, data2: np.ndarray, alpha=0.05):
+    data1 = data1[np.isnan(data1) == False]
+    data2 = data2[np.isnan(data2) == False]
+    mean1, mean2 = np.mean(data1), np.mean(data2)
+    n1, n2 = len(data1), len(data2)
+    std1, std2 = np.std(data1, ddof=1), np.std(data2, ddof=1)
+    return mean_hypothesis_test(mean1, std1, n1, mean2, std2, n2)
 
-# print(test_same_distribution('Q', 'RC1', 'F', 'HB3', 0.05, 5))
+def variance_hypothesis_test(s1, s2, n1, n2, alpha=0.05):
+    """How to use scipy.stats.f lmao"""
+    return 
 
+def hypothesis_testing(round):
+    all_maps_in_the_round = list(logit_dataset.columns)
+    all_maps_in_the_round = list(filter(lambda x: x.split("_")[0] == round, all_maps_in_the_round))
+
+    sus_pair = []
+    for i, type1 in enumerate(all_maps_in_the_round):
+        for j, type2 in enumerate(all_maps_in_the_round[i+1:]):
+            data1 = logit_dataset[type1].values
+            data2 = logit_dataset[type2].values
+            if np.all(np.isnan(data1)) or np.all(np.isnan(data2)):
+                continue
+            p_value, reject_null = hypothesis_raw_dataset(data1, data2)
+            print(type1, type2, p_value)
+            if reject_null:
+                sus_pair.append([type1, type2])
+
+    return sus_pair
+
+
+print(hypothesis_testing('SF'))
--- outlierdetection.py
+++ outlierdetection.py
@@ -4,7 +4,7 @@ from eda import map_label
 
 logit_dataset = pd.read_csv('4dm_logit.csv')
 
-def get_outlier(round, map_type, iqrmultipiler=1.5):
+def get_outlier_IQR(round, map_type, iqrmultipiler=1.5):
     series = logit_dataset[round + "_" + map_type]
     q1 = series.quantile(0.25)
     q3 = series.quantile(0.75)
@@ -14,35 +14,36 @@ def get_outlier(round, map_type, iqrmultipiler=1.5):
 
     return left_ol, right_ol
 
-outlier_data = []
-outlier_list = []
-for round in map_label.keys():
-    for map_type in map_label[round]:
-        lbl = round + "_" + map_type
-        left_ol, right_ol = get_outlier(round, map_type)
-        for ol in left_ol:
-            if ol in outlier_list:
-                idx = outlier_list.index(ol)
-                outlier_data[idx]['left_outlier'].append(lbl)
-                continue
-            outlier_data.append({
-                'player_name': ol,
-                'left_outlier': [lbl],
-                'right_outlier': []
-            })
-            outlier_list.append(ol)
-        
-        for ol in right_ol:
-            if ol in outlier_list:
-                idx = outlier_list.index(ol)
-                outlier_data[idx]['right_outlier'].append(lbl)
-                continue
-            outlier_data.append({
-                'player_name': ol,
-                'left_outlier': [],
-                'right_outlier': [lbl]
-            })
-            outlier_list.append(ol)
+if __name__ == "__main__":
+    outlier_data = []
+    outlier_list = []
+    for round in map_label.keys():
+        for map_type in map_label[round]:
+            lbl = round + "_" + map_type
+            left_ol, right_ol = get_outlier_IQR(round, map_type)
+            for ol in left_ol:
+                if ol in outlier_list:
+                    idx = outlier_list.index(ol)
+                    outlier_data[idx]['left_outlier'].append(lbl)
+                    continue
+                outlier_data.append({
+                    'player_name': ol,
+                    'left_outlier': [lbl],
+                    'right_outlier': []
+                })
+                outlier_list.append(ol)
+            
+            for ol in right_ol:
+                if ol in outlier_list:
+                    idx = outlier_list.index(ol)
+                    outlier_data[idx]['right_outlier'].append(lbl)
+                    continue
+                outlier_data.append({
+                    'player_name': ol,
+                    'left_outlier': [],
+                    'right_outlier': [lbl]
+                })
+                outlier_list.append(ol)
 
-outlier_dataframe = pd.DataFrame(outlier_data)
-outlier_dataframe.to_csv('outliers.csv', index=False)
+    outlier_dataframe = pd.DataFrame(outlier_data)
+    outlier_dataframe.to_csv('outliers.csv', index=False)
