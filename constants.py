70a23aa7c07568127c6b1c9d064a05426d5d3047
IndexError <lkycst@gmail.com>
2022-05-11 00:22:00 +0700

    constants.py change final_dataset to 4dm_dataset

--- constants.py
+++ constants.py
@@ -2,4 +2,4 @@ INTERESTED_BEATMAP_TYPE = ["RC", "HB", "LN"]
 ROUNDS = ["Q", "RO32", "RO16", "QF", "SF", "F", "GF"]
 PLAYER_DATA_COLS = ["player_name", "player_id", "country_code"]
 LOGIT_DATASET_DIR = "4dm_logit.csv"
-FOURDM_DATASET_DIR = "final_dataset.csv"
+FOURDM_DATASET_DIR = "4dm_dataset.csv"
