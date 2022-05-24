1985ef14cae873266c1ccdee4736e2749d034dca
IndexError <lkycst@gmail.com>
2022-05-24 21:46:57 +0700

    add collaborative filtering and utils functions

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -1,61 +1,37 @@
 import sqlite3
 import numpy as np
 import pandas as pd
-from sklearn.impute import KNNImputer
 from dataset import Dataset
+from utils import CollaborativeFiltering
+from utils import csv_to_sql
 
-interested_rounds = ['Q', 'RO32', 'RO16', 'QF', 'SF', 'F', 'GF']
-interested_types = ['LN', 'HB', 'RC', 'TB']
-_4dm = Dataset('4dm4.db')
-old_ds = _4dm.get_old_dataset(interested_rounds, interested_types, True)
-played = pd.notna(old_ds)
-mean = _4dm.query(f"SELECT player_name, avg(score_logit) as average_score from scores where round in {tuple(interested_rounds)} and beatmap_type in {tuple(interested_types)} GROUP BY player_name")
-old_ds = old_ds
-for player_name in mean['player_name']:
-    old_ds.loc[player_name] -= float(mean[mean['player_name'] == player_name]['average_score'])
+interested_rounds = ["Q", "RO32", "RO16", "QF", "SF", "F", "GF"]
+interested_types = ["LN", "HB", "RC", "TB"]
+_4dm_dataset = Dataset("4dm4.db")
+csv_dataset = _4dm_dataset.get_old_dataset(interested_rounds, interested_types, True)
+played = pd.notna(csv_dataset)
+X = csv_dataset.values
 
-knn = KNNImputer()
-res = knn.fit_transform(old_ds)
+cf_db = sqlite3.connect("4dm4_cf.db")
+cf_cursor = cf_db.cursor()
 
-new_ds = pd.DataFrame(res, index=old_ds.index, columns=old_ds.columns)
-for player_name in mean['player_name']:
-    new_ds.loc[player_name] += float(mean[mean['player_name'] == player_name]['average_score'])
 
-new_ds = new_ds.apply(lambda x: 1000000 / (1 + np.exp(-x)))
-new_sql = sqlite3.connect('4dm4_impute.db')
-cursor = new_sql.cursor()
+# User based CF
+cf = CollaborativeFiltering()
 
-def format_values(dict_values):
-    new_values = []
-    for val in dict_values:
-        if isinstance(val, str):
-            new_values.append(f"\'{val}\'")
-        else:
-            new_values.append(str(val))
-    return ", ".join(new_values)
+predict = cf.transform(X)
+predict = 1000000 / (1 + np.exp(-predict))
+predicted_value = pd.DataFrame(
+    predict, index=csv_dataset.index, columns=csv_dataset.columns
+)
+predicted_value.to_csv("4dm4_user_cf_predict.csv")
 
-def insert(table, data: dict):
-    sql = f'INSERT INTO {table} ({", ".join(data.keys())}) values ({format_values(list(data.values()))})'
-    cursor.execute(sql)
+# Item Based CF
 
-def impute_data_to_sql(full_dataset: pd.DataFrame, is_played: pd.DataFrame):
-    index = full_dataset.index
-    columns = full_dataset.columns
-
-    for col in columns:
-        round, map_type, map_tag = col.split("_")
-        for idx in index:
-            score = full_dataset.at[(idx, col)]
-            played = is_played.at[(idx, col)]
-            data = {
-                'player_name': idx,
-                'score': score,
-                'played': int(played),
-                'round': round,
-                'beatmap_type': map_type,
-                'beatmap_tag': map_tag
-            }
-            insert('scores', data)
-
-impute_data_to_sql(new_ds, played)
-new_sql.commit()
+predict_Item = cf.transform(X.T)
+predict_Item = 1000000 / (1 + np.exp(-predict))
+predict_Item = predict_Item.T
+predict_Item_value = pd.DataFrame(
+    predict, index=csv_dataset.index, columns=csv_dataset.columns
+)
+predict_Item_value.to_csv("4dm4_item_cf.predict.csv")
---
+++ utils/__init__.py
@@ -0,0 +1,2 @@
+from .cf import CollaborativeFiltering
+from .csv_to_sql import csv_to_sql
