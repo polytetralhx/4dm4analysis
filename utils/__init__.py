317fc6b217da7b7f5d7cc7add48197befe085eb2
IndexError <lkycst@gmail.com>
2022-06-09 21:59:53 +0700

    asfdkjawsdbfja

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,4 @@ from .csv_to_sql import csv_to_sql
 from .hypothesistesting import hypothesis_raw_dataset as two_means_t_test
 from .hypothesistesting import variance_raw_dataset as two_variances_f_test
 from .listfromgenerator import generate_list_functions_from_generator_functions
+from .dftransformer import get_table
